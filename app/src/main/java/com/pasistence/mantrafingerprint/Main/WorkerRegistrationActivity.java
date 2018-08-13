6f6c8120a00a809cd4d2833afb5bea36c363d70c
pranali lohave <pranalilohave94@gmail.com>
2018-08-13 18:30:26 +0530

    commit

--- app/src/main/java/com/pasistence/mantrafingerprint/Main/WorkerRegistrationActivity.java
+++ app/src/main/java/com/pasistence/mantrafingerprint/Main/WorkerRegistrationActivity.java
@@ -2,6 +2,7 @@ package com.pasistence.mantrafingerprint.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.pasistence.mantrafingerprint.R;
 
@@ -11,5 +12,6 @@ public class WorkerRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_registration);
+
     }
 }

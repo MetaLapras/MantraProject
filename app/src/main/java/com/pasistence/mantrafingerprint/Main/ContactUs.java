f6e01759ffe011d01cac05d0f0e7ab3633d4cfcb
pranali lohave <pranalilohave94@gmail.com>
2018-09-05 20:00:13 +0530

    commit

---
+++ app/src/main/java/com/pasistence/mantrafingerprint/Main/ContactUs.java
@@ -0,0 +1,15 @@
+package com.pasistence.mantrafingerprint.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pasistence.mantrafingerprint.R;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
